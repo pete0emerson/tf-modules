a8bb19c6f4a4f8c7bba207881990014a269d364a
Pete Emerson <pete@gruntwork.io>
2022-08-08 12:19:51 -0700

    Simple module

---
+++ local_file/outputs.tf
@@ -0,0 +1,7 @@
+output "filename" {
+	value = local_file.main.filename
+}
+
+output "content" {
+	value = local_file.main.content
+}
---
+++ local_file/variables.tf
@@ -0,0 +1,9 @@
+variable "filename" {
+	description = "Name of the file to create"
+	type = string
+}
+
+variable "content" {
+	description = "Content of the file to be created"
+	type = string
+}
